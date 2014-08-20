697873c502ca2641f36d1456e60d90a74ab36ca1
mchalain <marc.chalain@gmail.com>
2014-08-20 10:37:51 +0200

    item id must be uint32 and not int32

--- src/http.c
+++ src/http.c
@@ -126,7 +126,7 @@ upnp_http_get_info (void *cookie,
   }
 
   /* ask for anything else ... */
-  id = atoi (strrchr (filename, '/') + 1);
+  id = strtoul (strrchr (filename, '/') + 1, NULL, 10);
   item = vfs_get_item_by_id (dlna, id);
   if (!item)
     return HTTP_ERROR;
@@ -279,7 +279,7 @@ upnp_http_open (void *cookie,
                                       AVTS_DESCRIPTION, AVTS_DESCRIPTION_LEN);
   
   /* ask for anything else ... */
-  id = atoi (strrchr (filename, '/') + 1);
+  id = strtoul (strrchr (filename, '/') + 1, NULL, 10);
   item = vfs_get_item_by_id (dlna, id);
   if (!item)
     return NULL;
